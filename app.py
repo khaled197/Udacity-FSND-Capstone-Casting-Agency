9aad531b16f43c4699337fba2df8ca43123621b1
Khaled Nasr <khaled.n.leila@gmail.com>
2020-08-03 15:17:20 +0200

    error: invalid auth headers

--- app.py
+++ app.py
@@ -184,13 +184,10 @@ def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            try:
-                token = get_token_auth_header()
-                payload = verify_decode_jwt(token)
-                check_permissions(permission, payload)
-                return f(*args, **kwargs)
-            except:
-                print(sys.exc_info())
+            token = get_token_auth_header()
+            payload = verify_decode_jwt(token)
+            check_permissions(permission, payload)
+            return f(*args, **kwargs)
 
         return wrapper
     return requires_auth_decorator
